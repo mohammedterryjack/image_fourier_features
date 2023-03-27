9d9f3ea2fcfe1cc7849ef1d47f4a4062ed745b7b
mohammedterryjack <67234383+mohammedterryjack@users.noreply.github.com>
2023-03-27 10:19:51 +0100

    Update image_features.py

--- image_features.py
+++ image_features.py
@@ -6,17 +6,19 @@ from PIL import Image
 
 def image_similarity(image_a:Image, image_b: Image) -> float:
   return cosine_similarity(
-    a=featurise_image_data(image=image_a), 
-    b=featurise_image_data(image=image_b)
+    a=fourier_featurise_image_data(image=image_a), 
+    b=fourier_featurise_image_data(image=image_b)
   )
 
-def featurise_image_data(image: ndarray, max_image_size: int = 50) -> ndarray:
+def fourier_featurise_image_data(image: ndarray, max_image_size: int = 50) -> ndarray:
   grayscale_image = image.convert("L")
   thumbnail_image = grayscale_image.resize((max_image_size, max_image_size))
   fourier_image = fft2(thumbnail_image)
   fourier_features_complex = fourier_image.reshape(-1)
-  fourier_features = concatenate([fourier_features_complex.real, fourier_features_complex.imag])
-  return fourier_features
+  return concatenate([
+    fourier_features_complex.real, 
+    fourier_features_complex.imag
+  ])
 
 def cosine_similarity(a: ndarray, b: ndarray) -> float:
   return (a @ b.T) / (norm(a) * norm(b))
